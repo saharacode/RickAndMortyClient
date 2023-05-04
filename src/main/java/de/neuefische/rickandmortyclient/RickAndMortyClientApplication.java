3cc1af60345bc5e7ae36a933bab83f9e8a73b446
saharacode <johanndallmann@gmx.de>
2023-05-04 15:45:44 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/rickandmortyclient/RickAndMortyClientApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.rickandmortyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/rickandmortyclient/RickAndMortyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.rickandmortyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
