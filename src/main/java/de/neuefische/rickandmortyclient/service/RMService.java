c227d15c3dadc8e102e224e52388af666fd24eed
saharacode <johanndallmann@gmx.de>
2023-06-06 16:12:44 +0200

    integration-test for getAllCharacters added with mockwebserver

--- src/main/java/de/neuefische/rickandmortyclient/service/RMService.java
+++ src/main/java/de/neuefische/rickandmortyclient/service/RMService.java
@@ -2,7 +2,7 @@ package de.neuefische.rickandmortyclient.service;
 
 import de.neuefische.rickandmortyclient.model.RMCharacter;
 import de.neuefische.rickandmortyclient.model.RMCharacterCollection;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -16,14 +16,14 @@ public class RMService {
 
     private final WebClient webClient; // = WebClient.create("https://rickandmortyapi.com/api/"); // initialize webclient with standard uri which will be called from every method
 
-    public RMService(@Value ("${rick-and-morty-api-url}") String rmApiUrl){
+    public RMService(@Value("${rick-and-morty-api-url}") String rmApiUrl){
         this.webClient = WebClient.create(rmApiUrl);
     }
 
     public List<RMCharacter> getAllCharacters() {
         RMCharacterCollection response =
                 Objects.requireNonNull(webClient.get()
-                                .uri("character") // Spezifizierung der Uri auf genaue Adresse
+                                .uri("/character") // Spezifizierung der Uri auf genaue Adresse
                                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) // JSON als Content festlegen
                                 .retrieve() // hole mir die Daten!
                                 .toEntity(RMCharacterCollection.class) // Mappe sie bitte auf folgende Klasse
@@ -35,7 +35,7 @@ public class RMService {
     public List<RMCharacter> getAllAliveCharacters() {
         RMCharacterCollection response =
                 Objects.requireNonNull(webClient.get()
-                                .uri("character/?status=alive")
+                                .uri("/character/?status=alive")
                                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                 .retrieve()
                                 .toEntity(RMCharacterCollection.class)
--- src/test/java/de/neuefische/rickandmortyclient/controller/RickAndMortyControllerTest.java
+++ src/test/java/de/neuefische/rickandmortyclient/controller/RickAndMortyControllerTest.java
@@ -1,12 +1,21 @@
 package de.neuefische.rickandmortyclient.controller;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,8 +24,44 @@ class RickAndMortyControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    private static MockWebServer mockWebServer;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("rick-and-morty-api-url",() -> mockWebServer.url("/").toString());
+    }
+
+    @BeforeAll
+    static void startMockServer() throws Exception {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void stopMockServer() throws Exception {
+        mockWebServer.shutdown();
+    }
 
     @Test
-    void getAllCharacters() {
+    void getAllCharacters() throws Exception {
+        mockWebServer.enqueue(new MockResponse()
+                        .setHeader("Content-Type", "application/json")
+                .setBody("""
+                         {
+                              "results": [
+                                {
+                                  "id": "1",
+                                  "name": "Currywurst",
+                                  "status": "Alive",
+                                  "species": "Human"
+                                }
+                              ]
+                            }
+                        """));
+
+        mockMvc.perform(get("/ram/characters"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].id").value("1"))
+                .andExpect(jsonPath("[0].name").value("Currywurst"));
     }
 }
